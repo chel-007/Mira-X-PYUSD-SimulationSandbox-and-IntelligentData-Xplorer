676494e3c50047b6a712b5ee24db40e9f4f2d7c9
chel-007 <wstones025@gmail.com>
2025-04-09 17:08:12 +0100

    remove stakingData

--- app/api/stakingData/route.ts
+++ app/api/stakingData/route.ts
@@ -1,26 +1,27 @@
-import { request, gql } from 'graphql-request';
+// import { request, gql } from 'graphql-request';
 import { NextResponse } from 'next/server';
 
-const subgraphUrl = 'https://gateway.thegraph.com/api/57e9956c0fb785721d6c7141d45346f4/subgraphs/id/5zvR82QoaXYFyDEKLZ9t6v9adgnptxYpKpSbxtgVENFV';
+const subgraphUrl = 'https://gateway.thegraph.com/api/57e9956c0fb785721d6c7141d45346f4/subgraphs/id/apikey';
 
-export async function POST(req: Request) {
-  const { owner, pool } = await req.json();
-  const query = gql`
-    query GetPositions($owner: String!, $pool: String!) {
-      positions(where: {owner: $owner, pool: $pool}) {
-        id
-        liquidity
-        tickLower { tickIdx }
-        tickUpper { tickIdx }
-      }
-    }
-  `;
-  try {
-    const data = await request(subgraphUrl, query, { owner, pool });
-    console.log(data)
-    return NextResponse.json(data);
-  } catch (error) {
-    console.error('Subgraph error:', error);
-    return NextResponse.json({ error: 'Failed to fetch subgraph data' }, { status: 500 });
-  }
-}
+// export async function POST(req: Request) {
+//   const { owner, pool } = await req.json();
+//   const query = gql`
+//     query GetPositions($owner: String!, $pool: String!) {
+//       positions(where: {owner: $owner, pool: $pool}) {
+//         id
+//         liquidity
+//         tickLower { tickIdx }
+//         tickUpper { tickIdx }
+//       }
+//     }
+//   `;
+//   try {
+//     const data = await request(subgraphUrl, query, { owner, pool });
+//     console.log(data)
+//     return NextResponse.json(data);
+//   } catch (error) {
+//     console.error('Subgraph error:', error);
+//     return NextResponse.json({ error: 'Failed to fetch subgraph data' }, { status: 500 });
+//   }
+// }
+
